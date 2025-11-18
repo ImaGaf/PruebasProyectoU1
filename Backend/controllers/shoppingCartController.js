f829885f533b324241db15333822f4717be348b2
DennisonCc <dachalacan1@espe.edu.ec>
2025-11-17 23:12:45 -0500

    Refactor shopping cart controller to use shoppingCartService for database operations

--- Backend/controllers/shoppingCartController.js
+++ Backend/controllers/shoppingCartController.js
@@ -1,12 +1,10 @@
-
-const ShoppingCart = require('../models/shoppingCart');
+const shoppingCartService = require('../services/shoppingCartService');
 
 
 exports.create = async (req, res) => {
   try {
     console.log(req.body);
-    const cart = new ShoppingCart(req.body);
-    await cart.save();
+    const cart = await shoppingCartService.createShoppingCart(req.body);
     res.status(201).json(cart);
   } catch (err) {
     res.status(400).json({ error: err.message });
@@ -14,35 +12,31 @@ exports.create = async (req, res) => {
 };
 
 exports.getAll = async (req, res) => {
-  const carts = await ShoppingCart.find();
+  const carts = await shoppingCartService.getAllShoppingCarts();
   res.json(carts);
 };
 
 exports.getById = async (req, res) => {
-  const cart = await ShoppingCart.findOne({ idShoppingCart: req.params.id });
+  const cart = await shoppingCartService.getShoppingCartById(req.params.id);
   if (!cart) return res.status(404).json({ error: 'Not found' });
   res.json(cart);
 };
 
 exports.update = async (req, res) => {
-  const cart = await ShoppingCart.findOneAndUpdate(
-    { idShoppingCart: req.params.id },
-    req.body,
-    { new: true }
-  );
+  const cart = await shoppingCartService.updateShoppingCart(req.params.id, req.body);
   if (!cart) return res.status(404).json({ error: 'Not found' });
   res.json(cart);
 };
 
 exports.delete = async (req, res) => {
-  const cart = await ShoppingCart.findOneAndDelete({ idShoppingCart: req.params.id });
+  const cart = await shoppingCartService.deleteShoppingCart(req.params.id);
   if (!cart) return res.status(404).json({ error: 'Not found' });
   res.json({ message: 'Deleted' });
 };
 
 exports.getByCustomer = async (req, res) => {
   const customerId = req.params.customerId;
-  const cart = await ShoppingCart.findOne({ customer: customerId });
+  const cart = await shoppingCartService.getShoppingCartByCustomer(customerId);
   if (!cart) return res.status(404).json({ error: 'Carrito no encontrado' });
   res.json(cart);
 };
